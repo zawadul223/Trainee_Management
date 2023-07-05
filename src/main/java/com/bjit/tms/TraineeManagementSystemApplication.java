3d94b21e4dee2c1a30ae3c52e8136206be9d4879
Zawadul Islam <zawadul.islam@bjitacademy.com>
2023-07-05 20:23:31 +0600

    Initial skeleton of the project

---
+++ src/main/java/com/bjit/tms/TraineeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.bjit.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("lol");
+		SpringApplication.run(TraineeManagementSystemApplication.class, args);
+	}
+
+}
